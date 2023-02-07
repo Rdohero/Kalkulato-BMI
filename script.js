c5e06294063a03a752a55af49e3836b28d651660
Rdohero <rojaridho8888@gmail.com>
2023-02-07 19:20:59 +0700

    menambah pernyaataan2 dibawah pernyataan

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var masukkanberat = document.querySelector(".MasukkanBerat");
 var tombolmenghitung = document.querySelector(".Menghitung");
 var hasil = document.querySelector(".Hasil");
 var pernyataan = document.querySelector(".Pernyataan");
+var pernyataan2 = document.querySelector(".Pernyataan2");
 var BMI,tinggi,berat,tinggi2,fixed;
 
 tombolmenghitung.addEventListener("click", ()=>{
@@ -14,12 +15,16 @@ tombolmenghitung.addEventListener("click", ()=>{
 
     if(BMI < 18.5){
         pernyataan.innerText = "Berat badan kurang";
+        pernyataan2.innerText = "Sebaiknya makan makanan yang bergizi agar tubuh lebih terisi";
     }else if((BMI > 18.6) && (BMI < 23)){
         pernyataan.innerText = "Berat badan normal";
+        pernyataan2.innerText = "Tubuh anda sehat, pertahankan";
     }else if((BMI >= 23) && (BMI < 25)){
         pernyataan.innerText = "Berat badan berlebih";
+        pernyataan2.innerText = "Sebaiknya kurangilah makanan berkarbohidrat dan rajinlah berolahraga !!";
     }else if(BMI >=25){
         pernyataan.innerText = "Berat badan obesitas";
+        pernyataan2.innerText = "kurangilah makanan berkarbohidrat dan rajinlah berolahraga !!";
     }
 
     
